79569865295418eac6e766aa7f486b317c412655
eydeem <adamzieta@gmail.com>
2022-04-10 23:31:50 +0200

    finn

--- book/views.py
+++ book/views.py
@@ -3,7 +3,6 @@ import datetime
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from .models import Book
-from django.shortcuts import render
 import requests
 
 
